9ec885c300635e9b2f9cbc31b1d3f6ff2d8689ea
kimdasolll <ryurioka2@gmail.com>
2024-07-11 13:10:57 +0900

    프로젝트

---
+++ main.js
@@ -0,0 +1,16 @@
+$('.side_img').click(function () {
+  $('.side_bar').addClass('active');
+});
+$('.side_bar_close').click(function () {
+  $('.side_bar').removeClass('active');
+})
+
+$(function(){
+  $('.menu_box  .menu_coffee, .menu_box .menu_dessert, .menu_box .menu_deli').hide();
+  $('.menu_name a').click(function(){
+    $('.menu_box   .menu_coffee, .menu_box  .menu_dessert, .menu_box  .menu_deli').hide().filter(this.hash).fadeIn();
+    $('.menu_name a').removeClass('active');
+    $(this).addClass('active');
+    return false;
+  }).filter(':eq(0)').click();
+})
